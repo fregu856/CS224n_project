37265d5beaba2851eda67fc3b49fd8dd9c2751d2
fregu856 <fregu856@student.liu.se>
2017-01-31 11:39:00 -0800

    Test

---
+++ project_test.py
@@ -0,0 +1,10 @@
+import os 
+import re
+
+import tensorflow as tf
+import tensorflow.python.platform
+from tensorflow.python.platform import gfile
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
