52a73b76280b8fd912faddfdd9152ff1e7842346
fregu856 <fregu856@student.liu.se>
2017-02-05 16:01:04 -0800

    Trying to get a slightly better file structure.

---
+++ utilities.py
@@ -0,0 +1,23 @@
+import pickle
+import os
+
+def tokenize_caption(caption):
+    # load the vocabulary from disk:        
+    vocabulary = pickle.load(open(os.path.join(captions_dir, 
+                                               "vocabulary"))) 
+    
+    # tokenize the caption:
+    tokenized_caption = []
+    for word in caption:
+        if word == "<START>":
+            tokenized_caption.append(-1)
+        elif word == "<UNK>":
+            tokenized_caption.append(-2)
+        else:
+            word_index = vocabulary.index(word)
+            tokenized_caption.append(word_index)
+    
+    # convert into a numpy array:
+    tokenized_caption = np.array(tokenized_caption)
+    
+    return tokenized_caption
